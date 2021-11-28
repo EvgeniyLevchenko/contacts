1ced06374cd5f8b803623ba00a4ee786a48c9457
EvgeniyLevchenko <evgeniy.levchnko@gmail.com>
2021-11-28 22:32:08 +0200

    Update InfoViewController.swift

--- contacts/InfoViewController.swift
+++ contacts/InfoViewController.swift
@@ -8,27 +8,59 @@
 import UIKit
 
 class InfoViewController: UIViewController, UINavigationControllerDelegate {
-//FIX-ME: - private, private, private, ...
-    @IBOutlet weak var avatarImage: UIImageView!
-//FIX-ME: - enter?
-    @IBOutlet weak var nameLabel: UILabel!
-//FIX-ME: - enter?
-    @IBOutlet weak var mobileLabel: UILabel!
-//FIX-ME: - enter?
-    @IBOutlet weak var emailLabel: UILabel!
-//FIX-ME: - enter?
-    @IBOutlet weak var notesLabel: UILabel!
+
+    @IBOutlet private weak var avatarImage: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var mobileLabel: UILabel!
+    @IBOutlet private weak var emailLabel: UILabel!
+    @IBOutlet private weak var notesLabel: UILabel!
     
     var personInfo = Person()
-//FIX-ME: - enter?
     var isGoingForward: Bool = false
-//FIX-ME: - enter?
     var callback: ((Person) -> Void)?
 //FIX-ME: - action
     @IBAction func editButtonTapped(_ sender: Any) {
-//FIX-ME: - literals
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        if let editContactViewController = storyboard.instantiateViewController(identifier: "editContactViewController") as? EditContactViewController {
+        passDataToEditVC()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fillPersonInfo()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if !isGoingForward {
+            if let avatar = avatarImage.image,
+               let name = nameLabel.text,
+               let mobile = mobileLabel.text,
+               let email = emailLabel.text,
+               let notes = notesLabel.text {
+                let person = Person(name: name, phoneNumber: mobile, email: email, notes: notes, avatar: avatar)
+                callback?(person)
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        isGoingForward = false
+    }
+    
+    private func fillPersonInfo() {
+        avatarImage.image = personInfo.avatar
+        nameLabel.text = personInfo.name
+        mobileLabel.text = personInfo.phoneNumber
+        emailLabel.text = personInfo.email
+        notesLabel.text = personInfo.notes
+    }
+    
+    private func passDataToEditVC() {
+        let storyboardName = "Main"
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let vcIndetifier = "editContactViewController"
+        
+        if let editContactViewController = storyboard.instantiateViewController(identifier: vcIndetifier) as? EditContactViewController {
             var personInfoToPass = Person()
             if let name = nameLabel.text,
                let mobile = mobileLabel.text,
@@ -50,33 +82,5 @@ class InfoViewController: UIViewController, UINavigationControllerDelegate {
             isGoingForward = true
             show(editContactViewController, sender: nil)
         }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//FIX-ME: - method
-        avatarImage.image = personInfo.avatar
-        nameLabel.text = personInfo.name
-        mobileLabel.text = personInfo.phoneNumber
-        emailLabel.text = personInfo.email
-        notesLabel.text = personInfo.notes
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        if !isGoingForward {
-            if let avatar = avatarImage.image,
-               let name = nameLabel.text,
-               let mobile = mobileLabel.text,
-               let email = emailLabel.text,
-               let notes = notesLabel.text {
-                let person = Person(name: name, phoneNumber: mobile, email: email, notes: notes, avatar: avatar)
-                callback?(person)
-            }
-        }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        isGoingForward = false
     }
 }
